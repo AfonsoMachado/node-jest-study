bf91c472d26df6fc267ce781fe1065c15e5269f3
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-05 09:58:20 -0300

    configure express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("opa");
+import express from "express";
+
+const server = express();
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
